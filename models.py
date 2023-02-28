5fc11d7099e47cfa01b1efab62a15240d0a7432b
IsabelManiega <isabelmaniega@hotmail.com>
2023-02-28 22:03:03 +0100

    añadidos GET + POST

--- models.py
+++ models.py
@@ -1,1 +1,10 @@
-# TODO: Crearemos el modelo de los datos a aguardar
+# Crearemos el modelo de los datos a aguardar
+from pydantic import BaseModel
+
+# Modelo de datos:
+class Iris(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+    species: str
